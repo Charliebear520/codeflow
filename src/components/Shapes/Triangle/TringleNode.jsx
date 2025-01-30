d8c45156570c935e3406561fb05eab1261685622
charliebear <Charliebear520@users.noreply.github.com>
2025-01-30 13:41:35 +0800

    feat: add resizable functionality to Diamond and Triangle nodes

--- src/components/Shapes/Triangle/TringleNode.jsx
+++ src/components/Shapes/Triangle/TringleNode.jsx
@@ -1,14 +1,17 @@
-import { Handle,Position } from '@xyflow/react';
+import { Handle, Position, NodeResizer } from '@xyflow/react';
 import styles from './Triangle.module.css';
 
-
-function Triangle({ data }) {
+function Triangle({ data, selected }) {
     return (
       <div className={styles.triangleNode}>
+        {/* 可調整大小的框架 */}
+        <NodeResizer color="#ff0071" isVisible={selected} minWidth={80} minHeight={50} />
+
+        {/* 連接點 */}
         <Handle type="target" position={Position.Top} className={styles.handleTop}/>
         <Handle type="source" position={Position.Bottom} className={styles.handleBottom}/>
       </div>
     );
-  }
-  
-  export default Triangle;
+}
+
+export default Triangle;
