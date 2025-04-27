9cfe96118f87a26336e252540e7187ad78b98169
charliebear <Charliebear520@users.noreply.github.com>
2025-04-27 15:57:52 +0800

    feat: 更新OnlineCoding組件，新增Python代碼編輯功能，並在Stage2頁面中引入該組件以支持線上編程

--- client/src/pages/Stage2Page.jsx
+++ client/src/pages/Stage2Page.jsx
@@ -1,24 +1,25 @@
-import { Layout, Card, Button, Tabs, Collapse, Input } from 'antd';
-import StageSwitcher from '../components/StageSwitcher';
-import TopicStage2 from '../components/TopicStage2';
-import Answer from '../components/Answer';
-import Check from '../components/Check';
-import { Row, Col } from 'antd';
+import { Layout, Card, Button, Tabs, Collapse, Input } from "antd";
+import StageSwitcher from "../components/StageSwitcher";
+import TopicStage2 from "../components/TopicStage2";
+import Check from "../components/Check";
+import { Row, Col } from "antd";
+import OnlineCoding from "../components/OnlineCoding";
 
-export default function Stage2Page() { 
-    return (
-      <div>
-        <Row>
-          <Col span={6}>
-            <TopicStage2 />
-          </Col>
-          <Col span={12}>
-            <Answer />
-          </Col>
-          <Col span={6}>
-            <Check />
-          </Col>
-        </Row>
-      </div>
-    );
+export default function Stage2Page() {
+  return (
+    <div>
+      <Row>
+        <Col span={6}>
+          <TopicStage2 />
+        </Col>
+        <Col span={12}>
+        <OnlineCoding />
+        </Col>
+        <Col span={6}>
+          <Check />
+        </Col>
+      </Row>
+
+    </div>
+  );
 }
